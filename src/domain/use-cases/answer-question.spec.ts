215460513b873d032508fca61fbb953197084833
gabriel-vitebo <vitebo@outlook.com>
2024-06-05 13:48:13 -0300

    create repository

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,9 +1,16 @@
 import { expect, test } from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
+import { AnswerRepository } from '../repositories/answer-repository'
 
-test('Create an answer', () => {
-  const answerQuestion = new AnswerQuestionUseCase()
-  const answer = answerQuestion.execute({
+const fakeAnswersRepository: AnswerRepository = {
+  create: async (answer) => {
+    return
+  },
+}
+
+test('Create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+  const answer = await answerQuestion.execute({
     questionId: '1',
     instructorId: '1',
     content: 'New Answer'
